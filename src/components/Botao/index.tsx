ef963120ed458e7fc3e975e3fea0b45320d89a3e
Henrique Costa <henriquefcosta19@gmail.com>
2022-06-08 20:58:07 -0300

    Aula 1 curso React Router Alura

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,5 +1,5 @@
-import styles from "./Botao.module.scss";
+import styles from './Botao.module.scss';
 
 export default function Botao() {
-  return <button className={styles.botao}> Botão </button>;
+    return <button className={styles.botao}> Botão </button>;
 }
