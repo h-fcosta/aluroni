16c3448144920379fd3016635643755cfa0d209b
Henrique Costa <henriquefcosta19@gmail.com>
2022-06-09 21:27:31 -0300

    Aula 3 - Curso React Router Alura

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,27 +1,26 @@
-import cardapio from 'data/cardapio.json';
-import styles from './Inicio.module.scss';
+import cardapio from "data/cardapio.json";
+import styles from "./Inicio.module.scss";
 
 export default function Inicio() {
-    let pratosRecomendados = [...cardapio];
+  let pratosRecomendados = [...cardapio];
 
-    //Escolhendo 3 pratos aleatórios do cardápio com função sort()
-    pratosRecomendados = pratosRecomendados
-        .sort(() => 0.5 - Math.random())
-        .splice(0, 3);
-    return (
-        <section>
-            <h3 className={styles.titulo}>Recomendações da Cozinha</h3>
-
-            <div className={styles.recomendados}>
-                {pratosRecomendados.map((item) => (
-                    <div key={item.id} className={styles.recomendados}>
-                        <div className={styles.recomendados__imagem}>
-                            <img src={item.photo} alt={item.title} />
-                        </div>
-                        <button className={styles.recomendado__botao}>Ver mais</button>
-                    </div>
-                ))}
+  //Escolhendo 3 pratos aleatórios do cardápio com função sort()
+  pratosRecomendados = pratosRecomendados
+    .sort(() => 0.5 - Math.random())
+    .splice(0, 3);
+  return (
+    <section>
+      <h3 className={styles.titulo}>Recomendações da Cozinha</h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map((item) => (
+          <div key={item.id} className={styles.recomendados}>
+            <div className={styles.recomendados__imagem}>
+              <img src={item.photo} alt={item.title} />
             </div>
-        </section>
-    );
+            <button className={styles.recomendado__botao}>Ver mais</button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
